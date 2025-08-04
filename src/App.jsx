fe315a7e01381b37075770f3b6217ed61791723b
RISHABH SHAH <rnshah1997@gmail.com>
2025-08-04 19:33:34 -0400

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-import WelcomePage from './pages/Welcome.jsx';
-import ChallengesPage from './pages/Challenges.jsx';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import WelcomePage from "./pages/Welcome.jsx";
+import ChallengesPage from "./pages/Challenges.jsx";
 
 const router = createBrowserRouter([
-  { path: '/', element: <WelcomePage /> },
-  { path: '/challenges', element: <ChallengesPage /> },
+  { path: "/", element: <WelcomePage /> },
+  { path: "/challenges", element: <ChallengesPage /> },
 ]);
 
 function App() {
